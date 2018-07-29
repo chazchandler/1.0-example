b02ba3e9d054204662c824133e868730171157e2
Chaz Chandler <clc31@inbox.com>
2018-07-29 16:18:39 -0400

    Updated to serverless-ruby lambda support w/ ruby 2.5.1

--- index.rb
+++ index.rb
@@ -1,4 +1,7 @@
-require 'pp'
+#!./ruby/bin/ruby
+
+# require 'pp'
+require 'json'
 require 'active_record'
 require 'kaminari'
 require './z'
@@ -29,19 +32,33 @@ class DepartmentResource < ApplicationResource
   attribute :name, :string
 end
 
-employees = EmployeeResource.all({
-  sort: '-id',
-  filter: { age: { gt: 30 } },
-  page: { size: 10, number: 1 },
-  include: 'positions.department'
-})
+begin
+  employees = EmployeeResource.all({
+    sort: '-id',
+    filter: { age: { gt: 30 } },
+    page: { size: 10, number: 1 },
+    include: 'positions.department'
+  })
 
-employees.each do |e|
-  puts "#{e.first_name} | #{e.positions[0].title} | #{e.positions[0].department.name}"
+  # employees.each do |e|
+  #   puts "#{e.first_name} | #{e.positions[0].title} | #{e.positions[0].department.name}"
+  # end
+  #
+  # pp JSON.parse(employees.to_jsonapi)
+  # puts "\n\n"
+  # pp JSON.parse(employees.to_json)
+  # puts "\n\n"
+  # puts employees.to_xml
+
+  puts JSON.generate(
+    statusCode: 200,
+    body:       employees.as_json
+  )
+
+rescue Exception => e
+  puts JSON.generate(
+    statusCode: 500,
+    body:       e.message
+  )
+
 end
-
-pp JSON.parse(employees.to_jsonapi)
-puts "\n\n"
-pp JSON.parse(employees.to_json)
-puts "\n\n"
-puts employees.to_xml
